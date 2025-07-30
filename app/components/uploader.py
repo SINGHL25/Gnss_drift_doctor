f86f8b2fc16db68272ca7c16e51160a734fdff8d
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-30 13:26:59 +1000

    Update uploader.py

--- app/components/uploader.py
+++ app/components/uploader.py
@@ -1,1 +1,10 @@
+# ----------------- app/uploader.py -----------------
+import streamlit as st
+import pandas as pd
+
+def upload_csv():
+    return st.file_uploader("Upload GNSS Data CSV", type=["csv"])
+
+def load_data(file):
+    return pd.read_csv(file)
 
