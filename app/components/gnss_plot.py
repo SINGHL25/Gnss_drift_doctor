9fa77b144f5edeb5adffe6006ef73ecbb55ec165
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-30 15:54:54 +1000

    Update gnss_plot.py

--- app/components/gnss_plot.py
+++ app/components/gnss_plot.py
@@ -1,1 +1,14 @@
+import plotly.express as px
+
+def plot_location_scatter(df):
+    fig = px.scatter_mapbox(df, lat="lat_gnss", lon="lon_gnss", zoom=15,
+                            mapbox_style="open-street-map",
+                            title="GNSS Positions on Map")
+    return fig
+
+def plot_truth_vs_gnss(df):
+    fig = px.scatter(df, x="lon_gnss", y="lat_gnss", color_discrete_sequence=["blue"], label="GNSS",
+                     title="GNSS vs Ground Truth")
+    fig.add_scatter(x=df["lon_true"], y=df["lat_true"], mode="markers", name="Ground Truth", marker=dict(color="green"))
+    return fig
 
